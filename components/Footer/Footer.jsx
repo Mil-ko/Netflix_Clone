2610118726046e809b97d2a442b7589ae76ecec8
Mil-ko <milkesaaboma2357@gmail.com>
2026-01-31 01:46:21 +0300

    header and footer

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import './footer.css';
+
+function Footer() {
+  return (
+    <>
+    <div className="footer_outer_container">
+      <div className="footer_inner_container">
+        <div className="footer_icons">
+          <i class="fa-brands fa-facebook-f"></i>
+          <i class="fa-brands fa-youtube"></i>
+          <i class="fa-brands fa-instagram"></i>
+        </div>
+        <div className="footer_data">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help centers</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service_code">
+          <p>Service Code</p>
+        </div>
+        <div className="copy-right">
+          <p>&copy; 1997-2024 Netflix, Inc.</p>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}   
+export default Footer;  
---
+++ components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import './Header.css';
+// import NetflixLogo from "../../src/assets/images/logo.png"
+import netflixLogo from '../../src/assets/images/Netflix_Symbol_CMYK.png';
+function Header() {
+  return (
+    <div className="header_outer_container">
+        
+        <div className="header_container">
+
+         <div className="header_left">
+             <ul className="navbars">
+                <li><img src={netflixLogo} alt="" className="Logo" /></li>
+                <li className="navs">Netflix</li>
+                <li className="navs">Home</li>
+                <li className="navs">TVshows</li>
+                <li className="navs">Movies</li>
+                <li className="navs">Latest</li>
+                <li className="navs">Mylist</li>
+                <li className="navs">Browse by languages</li>
+             </ul>
+          </div>
+          <div className="header_right">
+               <ul>
+                <li><i class="fa-solid fa-magnifying-glass"></i></li>
+                <li><i class="fa-solid fa-bell"></i></li>
+                <li><i class="fa-solid fa-user-circle"></i></li>
+                <li><i class="fa-solid fa-caret-down"></i></li>
+               
+               </ul>
+          </div>
+         </div>
+      
+    </div>
+  );
+}       
+export default Header;
